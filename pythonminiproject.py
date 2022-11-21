65715140963719bb61afcfa9bd9f3c032c08a70e
YashAgnihotri <115009895+YashAgnihotri@users.noreply.github.com>
2022-11-21 21:34:35 +0530

    Update pythonminiproject.py

--- pythonminiproject.py
+++ pythonminiproject.py
@@ -3,7 +3,7 @@ hour=int(input("hour hand : ")) #taken input from the user for hour hand
 minute=int(input("minute hand : ")) #taken input from the user for minute hand
 hour1=(hour%12+minute/60)*30
 #hour%12 = 1 <= hour <=12
-#minute%60 = 0 <= minute <=59
+#minute/60 = 0 <= minute <=59
 min1=minute*6 #360//60 
 angle=abs(hour1-min1)
 print(angle)
