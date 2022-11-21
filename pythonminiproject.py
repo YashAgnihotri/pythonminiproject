cfd4dcc95d27027c86c8feedccb8df03c1ce8e4f
YashAgnihotri <115009895+YashAgnihotri@users.noreply.github.com>
2022-11-21 21:10:53 +0530

    Update pythonminiproject.py

--- pythonminiproject.py
+++ pythonminiproject.py
@@ -1,7 +1,7 @@
 # find the angle between minute hand and hour hand in clock
-hour=int(input("hour hand : "))
-minute=int(input("minute hand : "))
-hour1=(hour%12+minute/60)*30
-min1=minute*6
+hour=int(input("hour hand : ")) #taken input from the user for hour hand
+minute=int(input("minute hand : ")) #taken input from the user for minute hand
+hour1=(hour%12+minute/60)*30 #
+min1=minute*6 #360/60 
 angle=abs(hour1-min1)
 print(angle)
